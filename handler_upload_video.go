64ffeda37be91d011b3120275f565f32cae70718
Ruslan <flyingbrain01@gmail.com>
2025-10-29 22:33:20 +0000

    lesson 6 add video upload

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,133 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+
+	const maxMemory = 10 << 30
+
+	r.ParseMultipartForm(maxMemory)
+
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Can not find the video", err)
+		return
+	}
+
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't have permissions", nil)
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	t := header.Header.Get("Content-type")
+
+	mediatype, _, err := mime.ParseMediaType(t)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Can not get filetype", err)
+		return
+	}
+	if mediatype != "video/mp4" {
+		respondWithError(w, http.StatusInternalServerError, "allowed only image", nil)
+		return
+	}
+
+	filetype := strings.Split(mediatype, "/")
+	name := fmt.Sprintf("%s.%s", "tubely-upload", filetype[1])
+	tFile, err := os.CreateTemp("./samples", name)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "can not create file", err)
+	}
+
+	defer tFile.Close()
+	defer os.Remove(tFile.Name())
+
+	_, err = io.Copy(tFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "coppy error", err)
+	}
+
+	ratio, err := getVideoAspectRatio(tFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "", err)
+		return
+	}
+
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "", err)
+		return
+	}
+	if ratio == "16:9" {
+		ratio = "landscape"
+	}
+
+	if ratio == "9:16" {
+		ratio = "portrait"
+	}
+
+	s3FileName := ratio + "/" + hex.EncodeToString(b) + ".mp4"
+	tFile.Seek(0, io.SeekStart)
+	s3Params := s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &s3FileName,
+		Body:        tFile,
+		ContentType: &mediatype,
+	}
+
+	_, err = cfg.s3Client.PutObject(context.Background(), &s3Params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "s3 file uploading error", err)
+	}
+
+	url := fmt.Sprintf("http://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, s3FileName)
+	videoData.VideoURL = &url
+
+	if err := cfg.db.UpdateVideo(videoData); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
+}
---
+++ video_ratio.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"math"
+	"os/exec"
+)
+
+type Stream struct {
+	CodecType string `json:"codec_type"`
+	Width     int    `json:"width"`
+	Height    int    `json:"height"`
+}
+
+type FFProbeResult struct {
+	Streams []Stream `json:"streams"`
+}
+
+func getVideoAspectRatio(filepath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filepath)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("ffprobe error: %v, output: %s", err, out.String())
+	}
+
+	var result FFProbeResult
+	if err := json.Unmarshal(out.Bytes(), &result); err != nil {
+		return "", fmt.Errorf("failed to parse ffprobe output: %w", err)
+	}
+
+	var height, width int
+
+	for _, stream := range result.Streams {
+		if stream.CodecType == "video" {
+			width = stream.Width
+			height = stream.Height
+		}
+	}
+
+	actual := float64(width) / float64(height)
+	name := ""
+	sDiff := math.MaxFloat64
+
+	commonRatios := map[string]float64{
+		"1:1":  1.0,
+		"4:3":  4.0 / 3.0,
+		"16:9": 16.0 / 9.0,
+		"9:16": 9.0 / 16.0,
+		"21:9": 21.0 / 9.0,
+		"3:2":  3.0 / 2.0,
+		"5:4":  5.0 / 4.0,
+		"32:9": 32.0 / 9.0,
+	}
+
+	for rName, ratio := range commonRatios {
+		diff := math.Abs(ratio - actual)
+		if diff < sDiff {
+			name = rName
+			sDiff = diff
+		}
+	}
+
+	if name != "16:9" && name != "9:16" {
+		name = "other"
+	}
+
+	return name, nil
+}
